45df46fbdc0be1c48c853c55db20c8541246bbd0
Ayanali261773 <aspiringdeveloper261@gmail.com>
2025-06-20 20:26:26 +0500

    index .html , style.css with basic JS

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log(
+ Ayan
+)
+  document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for contacting me! I'll get back to you soon.");
+});
